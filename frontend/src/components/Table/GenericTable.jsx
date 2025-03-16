a69d98c6662dd15dc2fbd1adca17efeee560dea9
FatihErdgn <fatiherdogann099@gmail.com>
2025-03-16 20:13:38 +0300

    payment can be deleted, consultant filter changed to 20,50,100  and randevu yenile popup can be closed with outside click

--- frontend/src/components/Table/GenericTable.jsx
+++ frontend/src/components/Table/GenericTable.jsx
@@ -28,6 +28,7 @@ export default function GenericTable() {
           onChange={handleRowsPerPageChange}
           className="border border-gray-300 rounded p-1"
         >
+          <option value={10}>10</option>
           <option value={20}>20</option>
           <option value={50}>50</option>
           <option value={100}>100</option>
